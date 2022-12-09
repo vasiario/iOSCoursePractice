99de5d75e65db375445ae784f09c9397989fb43f
vasiario <vasiariov@gmail.com>
2022-12-09 15:38:59 +0700

    + Variable completed

--- Block 1/1.Variables - Constants. Primitive types_ Int, String, Bool.playground/Contents.swift
+++ Block 1/1.Variables - Constants. Primitive types_ Int, String, Bool.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*
+MARK: - Practice Variables - Constants. Primitive types_ Int, String, Bool.playground
+• Create variables or constants where you can store: your
+ age, title of your position, information about whether you are at home or not.
+• Change the values:
+  ○ age - add year
+  ○ profession name - add the word “Senior” before the name
+  ○ change your status to the opposite one
+• Print all the data to the console, embedding it in a sentence: “I am *** years old,
+  and I work at the position ***”
+*/
+
+var age = 24
+var myProfession = "IOS Developer"
+var inHome = false
+
+age += 1
+myProfession = "Senior \(myProfession) "
+//print(myProfession)
+inHome.toggle()
+
+print("I'm \(age) years old, and i work for a position \(myProfession)")
