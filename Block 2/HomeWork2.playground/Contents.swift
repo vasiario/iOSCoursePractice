4ee25cde0802224e18c216e13d437400a7cb9f8a
vasiario <vasiariov@gmail.com>
2022-12-26 11:36:46 +0700

    Added changes in HomeWork2 (updated)

--- Block 2/HomeWork2.playground/Contents.swift
+++ Block 2/HomeWork2.playground/Contents.swift
@@ -1,114 +1,126 @@
 import UIKit
 
+//MARK: -  Homework 2
 
-//MARK: - 1. Создайте класс Матрос (Sailor), в котором будет инициализатор, одно свойство - имя матроса и один метод "introduceMyself". При вызове этого метода, в консоль будет выводиться сообщение "Привет, меня зовут *имя матроса*!"
+/*
+ Классы
+ 1. Создайте класс Матрос (Sailor), в котором будет инициализатор, одно свойство - имя матроса и один метод "introduceMyself". При вызове этого метода, в консоль будет выводиться сообщение "Привет, меня зовут *имя матроса*!"
+ 2. Создайте класс Корабль, в котором будет инициализатор, два свойства: название корабля и массив “матросы”. Добавьте также функцию "introduceAll", которая будет выводить приветствие от имени всех матросов, после чего выводить сообщение "Мы с корабля *имя корабля*"
+ 3. Через инициализатор создайте 5 экземпляров класса Матрос и класс Корабль, в который передайте список созданных матросов. Вызовите функцию introduceAll. Вы должны получить в консоли приветствие от имени всех матросов и название корабля
+ */
 
 class Sailor {
-    var nameSailor: String
-    func introduceMyself() {
-        print("Hello, my name is \(nameSailor)")
+    let name: String
+    init(name: String) {
+        self.name = name
     }
-    init(nameSailor: String) {
-        self.nameSailor = nameSailor
-    }
-    convenience init() {
-        self.init(nameSailor: "")
+    func introduceMySelf() {
+        print("Hi, my name is \(name)")
     }
 }
-
-let sailor = Sailor()
-sailor.nameSailor = "Vanya"
-//print(sailor.nameSailor)
-
-//sailor.introduceMyself()
-
-
-//MARK: - Создайте класс Корабль, в котором будет инициализатор, два свойства: название корабля и массив “матросы”. Добавьте также функцию "introduceAll", которая будет выводить приветствие от имени всех матросов, после чего выводить сообщение "Мы с корабля *имя корабля*"
 
 class Ship {
-    var nameShip: String
-    var sailorsArray: [String]
+    let name: String
+    var arrayOfSailors: [Sailor] = []
+    init(name: String, arrayOfSailors: [Sailor]) {
+        self.name = name
+        self.arrayOfSailors = arrayOfSailors
+    }
     func introduceAll() {
-        for (_, sailorsArray) in sailorsArray.enumerated() {
-            print("Hello, my name is \(sailorsArray)")
+        for sailor in arrayOfSailors {
+            print("Hi, my name is \(sailor.name)")
         }
+        print("We are from boat named \(name)")
     }
-    init(nameShip: String, sailorsArray:[String]) {
-        self.nameShip = nameShip
-        self.sailorsArray = sailorsArray
-    }
-    convenience init() {
-        self.init(nameShip: "", sailorsArray:[""])
-}
-
 }
 
 
+var ship = Ship(name: "Atlantida", arrayOfSailors: [])
+ship.arrayOfSailors.append(Sailor.init(name: "Mark"))
+ship.arrayOfSailors.append(Sailor.init(name: "Gena"))
+ship.arrayOfSailors.append(Sailor.init(name: "Petr"))
+ship.arrayOfSailors.append(Sailor.init(name: "Vitya"))
+ship.arrayOfSailors.append(Sailor.init(name: "Gena"))
+//ship.introduceAll()
 
 
-//MARK: - 3. Через инициализатор создайте 5 экземпляров класса Матрос и класс Корабль, в который передайте список созданных матросов. Вызовите функцию introduceAll. Вы должны получить в консоли приветствие от имени всех матросов и название корабля
-
-let exemplar = Ship()
-exemplar.sailorsArray = ["Petyshan", "Gena", "Oleg", "Leha", "Pitya"]
-exemplar.nameShip = "Severostal"
-//exemplar.introduceAll()
 
 
 
 //MARK: - 4. Создайте класс Titanic, наследник класса Ship. И класс TitanicSeilor, наследник Seilor. Пусть TitanicSeilor будет иметь ту же функцию introduseMyself, но сообщение будет выводить на английском. Аналогично класс Titanic, только пусть его функция introduceAll выводит в на двух языках: то же сообщение что и Ship, а после - такое же сообщение на английском. Не меняйте существующие классы Ship и Seilor
- 
-class Titanic: Ship {
-    override func introduceAll() {
-        print("Hello, my name is \(sailorsArray)")
-        print("ENGHello, my name is \(sailorsArray)")
-    }
-}
 
-class TitanicSeilor: Sailor {
-    override func introduceMyself() {
-            print("ENG - Hello, my name is \(nameSailor)")
+final class Titanic: Ship {
+    override func introduceAll() {
+        super.introduceAll()
+        for sailor in arrayOfSailors {
+            print("ENG: Hi, my name is \(sailor.name)")
+        }
+        print("ENG: We are from boat named \(name)")
     }
+    }
+   
+let test = Titanic(name: ship.name, arrayOfSailors: [])
+test.arrayOfSailors = ship.arrayOfSailors
+//test.introduceAll()
+    
+ 
+
+
+
+final class TitanicSeilor: Sailor {
+    override func introduceMySelf() {
+        print("ENG: Hi, my name is \(name)")
+    }
+    
 }
-//let test = TitanicSeilor()
-//test.introduceMyself()
-//let test2 = Titanic()
-//test2.introduceAll()
+let test2 = TitanicSeilor(name: "Bob")
+
+test2.introduceMySelf()
 
 
 
 //MARK: - 5. Создайте класс Calculator, с функциями + - * /. Создайте класс CalculatorPro, который будет также вычислять процент от числа и возводить число в степен/
 
 class Calculator {
-    
-    func add(a: Double, b: Double) -> Double {
-        a + b
+    func plus(_ val1: Double, _ val2: Double) -> Double {
+        val1 + val2
     }
-    
-    func subtract(a: Double, b: Double) -> Double {
-        a - b
+    func minus(_ val1: Double, _ val2: Double) -> Double {
+        val1 - val2
     }
-    
-    func multiply(a: Double, b: Double) -> Double {
-        a * b
+    func multiply(_ val1: Double, _ val2: Double) -> Double {
+        val1 * val2
     }
-    
-    func divide(a: Double, b: Double) -> Double {
-        a / b
+    func divide(_ val1: Double, _ val2: Double) -> Double {
+        val1 / val2
     }
 }
 
+let test3 = Calculator()
+test3.plus(10, 10)
+test3.divide(10, 3)
+
 class CalculatorPro: Calculator {
-    
-    func degree(a: Double, degree b: Double) -> Double {
-        pow(a, b)
+    func getProcent(_ val1: Double, _ val2: Double) -> Double {
+        return Double((val1 * val2) / 100)
     }
-    
-    func residue(a: Int, b: Int) -> Int {
-        return a/100*b
+    func exponentiation(_ val1: Double, _ val2: Double) -> Double {
+       pow(val1, val2)
     }
 }
-//let test = CalculatorPro()
-//test.residue(a: 2, b: 30)
+print(CalculatorPro().getProcent(100, 3))
+print(CalculatorPro().exponentiation(5, 2))
+
+
+//5.1* Создайте класс SuperProCalc, наследник CalculatorPro, который будет вычислять сложные проценты для расчета суммы депозита на указанный период времени с указанной процентной ставкой
+
+class SuperProCalc: CalculatorPro {
+    func compoundInterest(numb1: Double, numb2: Double, numb3: Double) -> Double {
+        let result = pow(numb1 * (1 + numb2/100), numb3)
+        return result
+    }
+}
+
 
 //MARK: - 6. Создайте функцию которая принимает 1 параметр, процент заряда батареи. Пусть функция вернет сообщения для пользователя в зависимости от этого процента:
 /*● если 100%: “Устройство заряжено”,
@@ -118,14 +130,17 @@ class CalculatorPro: Calculator {
 В остальных случаях не выводите ничего. Используйте switch case.
 */
 //
-func chargeStatus(charge: Int) {
-    switch charge {
-    case 100: print("Devise is charged")
-    case 70...80: print("You need to charge the device for six hours")
 
-    case 20...40: print("Use battery charger")
-    case 0: print("Battary is low")
-    default: print("")
+func batteryProcent(procent: Int) -> String {
+    switch procent {
+    case 100: return "Battery is full now"
+    case 70...80: return "70-80%"
+    case 40...70: return "You need to charge your phone in next 4 hours"
+    case 20...40: return "Charge now please"
+    case 0: return "Your battery is empty"
+    default: return "Ops...Error"
     }
 }
-chargeStatus(charge: 100)
+
+let actualBatteryStatus = batteryProcent(procent: 68)
+print(actualBatteryStatus)
